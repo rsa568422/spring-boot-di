dfc57f6450ff4673b99098156466d867aebbe246
Roberto Sánchez <rsa568422@gmail.com>
2021-09-19 23:15:04 +0200

    Clase 33:
    Se agrega la clase IServicio para desacoplar el código.
    
    La clase MiServicio para a implementar el interface IServicio.
    
    Se cambia el tipo de MiServicio a IServicio en IndexController.

--- src/main/java/com/bolsadeideas/springboot/di/app/models/services/MiServicio.java
+++ src/main/java/com/bolsadeideas/springboot/di/app/models/services/MiServicio.java
@@ -2,9 +2,10 @@ package com.bolsadeideas.springboot.di.app.models.services;
 
 import org.springframework.stereotype.Component;
 
-@Component
-public class MiServicio {
-	
+@Component("miServicioSimple")
+public class MiServicio implements IServicio {
+
+	@Override
 	public String operacion() {
 		return "ejecutando algún proceso importante...";
 	}
