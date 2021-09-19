864d03197f8272ba2de3edc91990aa85a79b71ee
Roberto Sánchez <rsa568422@gmail.com>
2021-09-19 22:48:18 +0200

    Clase 31:
    Se agrega servicio de prueba.
    
    Se hace uso del servicio en el controlador sin hacer uso de inyección de
    dependencias.
    
    Se agrega la vista index.html.

---
+++ src/main/java/com/bolsadeideas/springboot/di/app/models/services/MiServicio.java
@@ -0,0 +1,9 @@
+package com.bolsadeideas.springboot.di.app.models.services;
+
+public class MiServicio {
+	
+	public String operacion() {
+		return "ejecutando algún proceso importante...";
+	}
+
+}
