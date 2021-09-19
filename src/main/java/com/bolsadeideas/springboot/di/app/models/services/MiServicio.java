a8b265b792d342f96b88ea73e9cb4bc0fee9f51d
Roberto Sánchez <rsa568422@gmail.com>
2021-09-20 00:12:14 +0200

    Clase 36:
    Se agrega la clase MiServicioComplejo que también implementa a
    IServicio.
    
    Se agrega la etiqueta @Primary a la clase MiServicio de manera que
    Spring utilizará esta clase a la hora de injectar IServicio a menos que
    se le indique otra clase mediante @Qualifier.

--- src/main/java/com/bolsadeideas/springboot/di/app/models/services/MiServicio.java
+++ src/main/java/com/bolsadeideas/springboot/di/app/models/services/MiServicio.java
@@ -1,13 +1,15 @@
 package com.bolsadeideas.springboot.di.app.models.services;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
+@Primary
 @Component("miServicioSimple")
 public class MiServicio implements IServicio {
 
 	@Override
 	public String operacion() {
-		return "ejecutando algún proceso importante...";
+		return "ejecutando algún proceso importante simple...";
 	}
 
 }
